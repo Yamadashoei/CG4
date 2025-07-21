02098343f0c64bc368d411a0a2f95036e8f3b8cf
Yamadashoei <k023g0053@g.neec.ac.jp>
2025-07-21 14:05:19 +0900

    画像一枚追加

--- DirectXGame/Stage.cpp
+++ DirectXGame/Stage.cpp
@@ -12,12 +12,13 @@ Stage::~Stage() {
 }
 
 void Stage::Initialize() {
-	uint32_t tex = TextureManager::Load("./Resources/bg.png");
+	uint32_t tex1 = TextureManager::Load("./Resources/bg1.png");
+	uint32_t tex2 = TextureManager::Load("./Resources/bg2.png");
 
-	bgSprite1_ = Sprite::Create(tex, {0, 0});
+	bgSprite1_ = Sprite::Create(tex1, {0, 0});
 	bgSprite1_->SetSize({bgWidth_, 720.0f});
 
-	bgSprite2_ = Sprite::Create(tex, {bgWidth_, 0});
+	bgSprite2_ = Sprite::Create(tex2, {bgWidth_, 0});
 	bgSprite2_->SetSize({bgWidth_, 720.0f});
 }
 
@@ -32,9 +33,6 @@ void Stage::Update() {
 	bgSprite1_->SetPosition({scrollX_, 0});
 	bgSprite2_->SetPosition({scrollX_ + bgWidth_, 0});
 
-	//// 1枚目は通常、2枚目は反転
-	//bgSprite1_->SetIsFlipX(false);
-	//bgSprite2_->SetIsFlipX(true); // ←ここで反転！
 	bgSprite1_->SetPosition({scrollX_, 0.0f});
 	bgSprite2_->SetPosition({scrollX_ + 1280.0f, 0.0f});
 
