1e2b28d5f8f9c184fe6832bbdf69153488acf606
聖瑛 山田 <k023g0053@g.neec.ac.jp>
2025-08-09 18:48:55 +0900

    完了

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -7,6 +7,7 @@ using namespace KamataEngine;
 Player::Player() {}
 Player::~Player() {}
 
+//プレイヤー
 void Player::Initialize(Model* model, Vector3 position, Vector3 velocity) {
 	assert(model);
 	playerModel_ = model;
